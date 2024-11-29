d7a6df1af8be0451724d4757e304705f78e4f691
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2024-11-29 04:53:45 +0200

    added a few new content type scanning support

--- app/src/main/java/com/example/qr_reader_app/ui/MainActivity.java
+++ app/src/main/java/com/example/qr_reader_app/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.qr_reader_app.ui;
 
 import android.Manifest;
-import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,19 +11,12 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.PackageManagerCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.qr_reader_app.R;
 import com.example.qr_reader_app.databinding.ActivityMainBinding;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
@@ -54,41 +46,83 @@ public class MainActivity extends AppCompatActivity {
     });
 
     private void setResult(String contents) {
-        //check url validity
-        if (contents.startsWith("http://") || contents.startsWith("https://")) {
-            //alert window to confirm redirect
-            new AlertDialog.Builder(this)
-                    .setTitle("Confirm redirect")
-                    .setMessage("Do you want to open this link?\n"+contents)
-                    .setPositiveButton("Yes", (dialog, which) -> {
-                        //open url in browser
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contents));
-                        startActivity(browserIntent);
-                    })
-                    .setNegativeButton("No", (dialog, which) -> {
-                        dialog.dismiss();
-                    })
-                    .show();
+        //check content type
+        if (contents.endsWith(".mp3") || contents.endsWith(".mp4")) {
+            //media QR Code (MP3/MP4)
+            handleMediaQRCode(contents);
+        } else if (contents.endsWith(".pdf")) {
+            //PDF QR Code
+            handlePDFQRCode(contents);
+        } else if (contents.startsWith("http://") || contents.startsWith("https://")) {
+            //URL in browser or show content as text
+            handleURLQRCode(contents);
         } else {
-            int maxSymbols = 75;
-            if (contents.length() > maxSymbols) {
-                //formatting too long message
-                String truncatedMessage = contents.substring(0, maxSymbols-2) + "...";
-                binding.textResult.setText("«" + truncatedMessage + "»");
-
-                //notify the user the message is too long
-                Toast.makeText(this, "Message is too long", Toast.LENGTH_SHORT).show();
-
-                //click listener to show the full message
-                binding.textResult.setVisibility(View.VISIBLE);
-                binding.viewFullMessage.setVisibility(View.VISIBLE);
-                binding.viewFullMessage.setOnClickListener(view -> showFullMessageDialog(contents));
-            } else {
-                binding.textResult.setText("«" + contents + "»");
-                binding.textResult.setOnClickListener(null);
-            }
-            binding.textResult.setVisibility(View.VISIBLE);
+            //text content
+            handleTextQRCode(contents);
         }
+        binding.textResult.setVisibility(View.VISIBLE);
+    }
+
+    private void handlePDFQRCode(String contents) {
+        //open pdf
+        Intent pdfIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contents));
+        pdfIntent.setDataAndType(Uri.parse(contents), "application/pdf");
+
+        //check if there's an app to handle PDF files
+        if (pdfIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(pdfIntent);
+        } else {
+            Toast.makeText(this, "No PDF viewer app found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void handleMediaQRCode(String contents) {
+        //open the media player or url
+        Intent mediaIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contents));
+
+        //check if the media is supported
+        if (mediaIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mediaIntent);
+        } else {
+            Toast.makeText(this, "No suitable app found to open media", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void handleTextQRCode(String contents) {
+        int maxSymbols = 75;
+        if (contents.length() > maxSymbols) {
+            //formatting too long message
+            String truncatedMessage = contents.substring(0, maxSymbols-2) + "...";
+            binding.textResult.setText("«" + truncatedMessage + "»");
+
+            //notify the user the message is too long
+            Toast.makeText(this, "Message is too long", Toast.LENGTH_SHORT).show();
+
+            //click listener to show the full message
+            binding.textResult.setVisibility(View.VISIBLE);
+            binding.viewFullMessage.setVisibility(View.VISIBLE);
+            binding.viewFullMessage.setOnClickListener(view -> showFullMessageDialog(contents));
+        } else {
+            binding.textResult.setText("«" + contents + "»");
+            binding.textResult.setOnClickListener(null);
+        }
+        binding.textResult.setVisibility(View.VISIBLE);
+    }
+
+    private void handleURLQRCode(String contents) {
+        //alert window to confirm redirect
+        new AlertDialog.Builder(this)
+                .setTitle("Confirm redirect")
+                .setMessage("Do you want to open this link?\n"+contents)
+                .setPositiveButton("Yes", (dialog, which) -> {
+                    //open url in browser
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contents));
+                    startActivity(browserIntent);
+                })
+                .setNegativeButton("No", (dialog, which) -> {
+                    dialog.dismiss();
+                })
+                .show();
     }
 
     private void showFullMessageDialog(String message) {
